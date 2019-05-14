ebeef72fbceb6d286e556ec80be1a6640e948818
LukeZzz <lollukasschwab@gmail.com>
2019-05-14 13:56:21 +0200

    commm

--- express-api/src/arduino/serialport.js
+++ express-api/src/arduino/serialport.js
@@ -1,22 +1,29 @@
 const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
 
 var arduinoPort = new SerialPort('COM3', {
 	autoOpen: false,
 	dataBits: 8,
 	stopBits: 1,
 	baudRate: 9600
-}, false);
+});
+
+arduinoPort.pipe(new Readline());
 
 arduinoPort.open(err => {
 	err ? console.error(err) : console.log('opened port COM3');
 });
 
-arduinoPort.on('data', data => console.log('Data: ', data));
+//arduinoPort.on('data', data => console.log('Data: ', data));
 
-/* { comName: 'COM3',
-    manufacturer: 'Microsoft',
-    serialNumber: '7553334343635141C071',
-    pnpId: 'USB\\VID_2341&PID_0043\\7553334343635141C071',
-    locationId: 'Port_#0006.Hub_#0003',
-    vendorId: '2341',
-    productId: '0043' } */
+var text = '';
+
+arduinoPort.on('data', data => text += data.toString());
+
+setTimeout(() => {
+	arduinoPort.close(err => console.error(err));
+}, 1000);
+
+arduinoPort.on('close', () => {
+	console.log('Text: ', text);
+});
